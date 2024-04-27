bf4066115e82313c658213385f60fb19d072994d
adamhinton <adam.d.hinton@gmail.com>
2024-04-27 12:47:58 -0400

    messing around with styling

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
 import MyText from './components/MyText/MyText';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <MyText />
+      <View style={{backgroundColor: 'blue', height: 100}}>
+        <Text>This is going to be our header container</Text>
+      </View>
+
+      <View style={{backgroundColor: 'grey'}}>
+        <MyText />
+        <MyText />
+        <MyText />
+      </View>
+
+      <View>
+        <View>
+          <Text>All rights reserved</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
--- components/MyText/MyText.js
+++ components/MyText/MyText.js
@@ -3,7 +3,11 @@ import {Text} from 'react-native';
 import style from './style';
 
 const MyText = () => {
-  return <Text style={style.text}>Hello, React Native World!</Text>;
+  return (
+    <Text style={[style.text, {color: 'green'}]}>
+      Hello, React Native World!
+    </Text>
+  );
 };
 
 export default MyText;
