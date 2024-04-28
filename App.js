8b71f85e5a8dfa1dd5fd660ea53f5e2ae22da543
adamhinton <adam.d.hinton@gmail.com>
2024-04-27 20:53:58 -0400

    my first RN alert

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import MyText from './components/MyText/MyText';
 
 const App = () => {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <MyText />
+    </SafeAreaView>
+  );
 };
 
 export default App;
--- components/MyText/MyText.js
+++ components/MyText/MyText.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-alert */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text} from 'react-native';
 import style from './style';
 
 const MyText = () => {
+  const handleTextClick = () => {
+    alert('The text has been clicked');
+  };
+
   return (
-    <Text style={[style.text, {color: 'green'}]}>
+    <Text
+      style={[style.text, {color: 'green'}]}
+      onPress={() => handleTextClick()}>
       Hello, React Native World!
     </Text>
   );
